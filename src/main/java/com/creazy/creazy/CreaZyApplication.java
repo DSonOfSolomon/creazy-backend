c5a0db0eb70b8fc6f5e17911d84ec988b7a06691
DChioma(D.sonofSolomon) <122977720+DannyCanary@users.noreply.github.com>
2025-12-29 01:31:07 +0000

    Initial Spring Boot project setup

---
+++ src/main/java/com/creazy/creazy/CreaZyApplication.java
@@ -0,0 +1,23 @@
+package com.creazy.creazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CreaZyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CreaZyApplication.class, args);
+	}
+	@GetMapping("/creaZy")
+	public String CreaZy(){
+
+
+        return "Welcome to creaZy 👋";
+    }
+
+}
---
+++ src/test/java/com/creazy/creazy/CreazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creazy.creazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
